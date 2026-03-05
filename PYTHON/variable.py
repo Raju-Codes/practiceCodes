7612f3f268aef464270144ac2849d828b9f6f518
Raju-Codes <rajub.codes@gmail.com>
2026-03-06 00:08:17 +0530

    data types

---
+++ PYTHON/variable.py
@@ -0,0 +1,17 @@
+# Numerics data types in python
+# int data type
+var1 = 10
+print(var1, type(var1))
+# float data type
+var2 = 20.0
+print(var2, type(var2))
+# Complex data type
+var3 = 2 + 82j
+print(var3, type(var3))
+
+# String
+var4 = "String"
+print(var4, type(var4))
+# Array
+var5 = [10, 11, 2.1, "hello"]
+print(var5, type(var5))
