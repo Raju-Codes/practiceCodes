028e82a4e27f3164d1a3badf8a0d02837cfea064
Raju-Codes <rajub.codes@gmail.com>
2026-03-20 00:47:04 +0530

    Java basics

---
+++ JAVA/src/basicOperation/Hello.java
@@ -0,0 +1,10 @@
+package basicOperation;
+
+public class Hello {
+    static void main(String[] args) {
+        System.out.println("Hello");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Helo " + i);
+        }
+    }
+}
