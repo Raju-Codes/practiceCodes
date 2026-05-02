4d70f4558232bdd71333f420d24c218a91f27974
Raju-Codes <rajub.codes@gmail.com>
2026-05-03 00:28:55 +0530

    java

--- JAVA/src/basicOperation/Hello.java
+++ JAVA/src/basicOperation/Hello.java
@@ -1,10 +1,27 @@
 package basicOperation;
 
+import java.util.Scanner;
+
 public class Hello {
     static void main(String[] args) {
-        System.out.println("Hello");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Helo " + i);
+        Scanner sc = new Scanner(System.in);
+//        int x;
+//        int y;
+//        int z;
+//        x = y = z=10;
+//        System.out.println("Value of x "+ x);
+//        System.out.println("Value of y "+ y);
+//        System.out.println("Value of z "+ z);
+//        z=100;
+//        System.out.println("Value of x "+ x);
+//        System.out.println("Value of y "+ y);
+//        System.out.println("Value of z "+ z);
+
+        int[] myArray = new int [5];
+        for (int i=0; i < myArray.length; i++){
+            myArray[i]=sc.nextInt();
         }
+        for (int i=0; i < myArray.length; i++){
+            System.out.print(myArray[i]+ " ");        }
     }
 }
