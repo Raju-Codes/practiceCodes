c1da5d6bec2c53179f9c2f0205addf78db235ca7
Raju-Codes <rajub.codes@gmail.com>
2026-07-28 22:54:11 +0530

    Python Basics

---
+++ PYTHON/Basics/Loops.py
@@ -0,0 +1,14 @@
+# for iterator in seq:
+# Statements
+# n = int(input("please enter the Number"))
+# sum = 0
+# for i in range(1,n+1):
+#     sum += i
+# print(sum)
+#
+# for i in range(2,10,2):
+#     print(i)
+i = 0
+while i<3:
+    print(i,"Hellow World")
+    i+=1
--- PYTHON/Basics/String.py
+++ PYTHON/Basics/String.py
@@ -1,6 +1,21 @@
 s1 = """Hello """
 print(len(s1))
 #Indexing in Python
-s2 = "python"
+s2 = "python world"
+print(len(s2))
 print(s2[1],s2[-1])
 print(s1+s2)
+
+# To find the length of a string
+print(len(s1), "length of the string")
+
+# To get single char with index number
+print("single letter",s2[1],"last character",s2[-1])
+
+# to print each letter in loop
+for i in s2:
+    print(i)
+
+# Operation in Strings
+# Slicing String
+print(s2[1:4:2])
