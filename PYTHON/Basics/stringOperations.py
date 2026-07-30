b9a0492f6fde0e371a02a9ca593fd5c15b74f959
Raju-Codes <rajub.codes@gmail.com>
2026-07-30 23:35:52 +0530

    Python Basics

--- PYTHON/Basics/stringOperations.py
+++ PYTHON/Basics/stringOperations.py
@@ -7,7 +7,6 @@
 # language= "Python"
 # version = "3.1.4"
 # print(language+version)
-from PYTHON.Basics.String import s2
 
 # #Multiplication is possible in Python
 # s1="Python"
@@ -34,3 +33,33 @@ from PYTHON.Basics.String import s2
 # s3=s1.strip()
 # print(s3=="Hello World")
 
+# # To replace the String with for temporary or assign to new variable.
+# string = "Python World Java then Java Java"
+# print(string.replace("Java", "c+++",1))
+# print(string)
+# # with 1 being mentioned it is going to replace only the first occurrence of mentioned value
+
+# # To count the data represented
+# string1 = "Hello Python to new world"
+# string2 = "o"
+# print(string1.count(string2))
+# # It is going to work even if the data is upper or lower
+
+# # upper() lower(), title(), capitalize()
+# string1 = "learning Python programming Language"
+# print(string1.capitalize())
+# # To Capitalize first char of entire String.
+# print(string1.title())
+# # To capitalize every first char in each word.
+# print(string1.upper())
+# #to capitalize entire char to be capitalized.
+# print(string1.lower())
+# #to keep small letter entire char in full strings.
+
+# Starting and ending of a string.
+# startswith() endswith()
+
+# string1= "learning Python programming Language"
+# print(string1.endswith("e"))
+
+# print(string1.startswith("l"))
