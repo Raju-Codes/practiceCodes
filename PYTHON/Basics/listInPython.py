cc8a0701d8bb20356b6a4394d215c08efab3db87
Raju-Codes <rajub.codes@gmail.com>
2026-08-03 22:59:32 +0530

    Python Basics

---
+++ PYTHON/Basics/listInPython.py
@@ -0,0 +1,39 @@
+# #Operations on Lists
+# from operator import length_hint
+#
+# lists1 = [12,13,15,12,90,11]
+# print(lists1)
+#
+# #Reversing the initial list
+# lists1.reverse()
+# print("reversing in the initial lists",lists1)
+#
+# #Sorting the data in ascending Order
+# lists1.sort()
+# print("Sorted in ascending order",lists1 )
+#
+# #Sorting in descending order
+# lists1.sort(reverse = True)
+# print("Sorting in descending order",lists1)
+#
+# # Counting the occurrence of particular number
+# a=12
+# print(lists1.count(a), "times this mum is mentioned in List",a)
+#
+# # To verify if the number is present inside the list
+# print(11 in lists1)
+#
+# # To fund the min value of the list
+# print("Min value in list ",min(lists1))
+#
+# # To find the Max value of the List
+# print("Max number in list",max(lists1))
+#
+# # To find the Sum of the list
+# print("Sum of the list",sum(lists1))
+#
+# #To find the length of the lists
+# print("Length of the List",len(lists1))
+
+# Nested list
+# list containing the list inside in it
