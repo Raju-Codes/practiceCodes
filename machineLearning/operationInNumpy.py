63e39adb771e346590f4b4765668fa2556e84a48
Raju-Codes <rajub.codes@gmail.com>
2026-08-30 00:35:56 +0530

    Introduction to JS

---
+++ machineLearning/operationInNumpy.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+b = np.array([10, 11, 12, 13, 15, 16, 17, 18, 19])
+print("Addition of two array", a + b)
+
+print("Subtraction of two array", a - b)
+print("Multiplication of two array", a * b)
+print("Division of two array", b / a)
+print("Pow of two array", a ** 2)
+
+# Comparison statement
+print("Comparison statement", a==7)
+
+# Universal function
+print("universal function",np.sqrt(a))
+print(np.sin([0, np.pi/2]))
---
+++ machineLearning/operationInPandas.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# to print series type of data.
+s= pd.Series(["Raju","27","raju@gmail.com","Bangalore Marathalli","QHS3278GHS",12345],index=['Name','Age','Mail','address','GST','sellerID'])
+print(s)
+
+# to print data in rows and column format
+data = {"Name":["Rajesh", "Bob"],"Age":[23,25],"Mail" :["rajesh@gmail.com","bob@gmail.com"],"Address":["Bangalore", "Andhra Pradesh"],"GST":["TYDG123456TY","HDFT736212GG"],"sellerID":[64237,3217]}
+df = pd.DataFrame(data)
+print(df)
+
+# Selecting rows and columns in Pandas
+print("Selecting rows and columns")
+print(df[['Name',"Age"]])
+
+# To select rows in pandas
+print("To select rows in pandas")
+print(df.iloc[0])
+
+# To print the range of Rows
+print(df[0:2])
+print("df.iloc[0:2]")
+print(df[(df["Age"]>24 & (df["Age"]<26))])
+
